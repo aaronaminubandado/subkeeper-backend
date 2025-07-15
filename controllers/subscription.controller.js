c2d01a36e6a0c9f52ebe51bcf8647cdd5e11d8c5
Aaron Bandado <aaron.aminu1@gmail.com>
2025-07-15 10:33:26 +0300

    add workflows for email
    created workflow routes and controller to send reminders

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,5 +1,7 @@
 import { json } from "express";
 import Subscription from "../models/subscription.model.js";
+import { workflowClient } from "../config/upstash.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
 	try {
@@ -8,7 +10,20 @@ export const createSubscription = async (req, res, next) => {
 			user: req.user._id,
 		});
 
-		res.status(201).json({ success: true, data: subscription });
+		const { workflowRunId } = await workflowClient.trigger({
+			url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+			body: {
+				subscriptionId: subscription.id,
+			},
+			headers: {
+				"content-type": "application/json",
+			},
+			retries: 0,
+		});
+
+		res
+			.status(201)
+			.json({ success: true, data: { subscription, workflowRunId } });
 	} catch (e) {
 		next(e);
 	}
@@ -24,7 +39,7 @@ export const getUserSubscriptions = async (req, res, next) => {
 
 		const subscriptions = await Subscription.find({ user: req.params.id });
 
-        res.status(200).json({ success: true, data: subscriptions});
+		res.status(200).json({ success: true, data: subscriptions });
 	} catch (e) {
 		next(e);
 	}
